fbb6f1782cb8e0085067753b55ba78848fdbad2c
Artem <artem@wildapricot.com>
2021-03-11 14:15:32 +0300

    add weahter view helper unit test

--- WeatherAppViperUIKit/WeatherAppViperUIKitTests/WeatherModule_Tests.swift
+++ WeatherAppViperUIKit/WeatherAppViperUIKitTests/WeatherModule_Tests.swift
@@ -36,4 +36,64 @@ class WeatherModule_Tests: XCTestCase {
         XCTAssertEqual(result.0, expectedMainLocationString, "Incorrect mainLocationString")
         XCTAssertEqual(result.1, expectedAdditionalLocationString, "Incorrect additionalLocationString")
     }
+
+    func test_parse_placemark_with_empty_locality() {
+        // setup
+        let initLocality: String? = nil
+        let initAdministrativeArea = "Moscow Oblast"
+        let initCountry = "Russia"
+
+        let expectedMainLocationString = "Moscow Oblast, Russia"
+        let expectedAdditionalLocationString = ""
+
+        // action
+        let result = WeatherViewHelper.prepareLocationContent(
+                locality: initLocality,
+                administrativeArea: initAdministrativeArea,
+                country: initCountry)
+
+        //assert
+        XCTAssertEqual(result.0, expectedMainLocationString, "Incorrect mainLocationString")
+        XCTAssertEqual(result.1, expectedAdditionalLocationString, "Incorrect additionalLocationString")
+    }
+
+    func test_parse_placemark_with_just_country() {
+        // setup
+        let initLocality: String? = nil
+        let initAdministrativeArea: String? = nil
+        let initCountry = "Russia"
+
+        let expectedMainLocationString = "Unknown, Russia"
+        let expectedAdditionalLocationString = ""
+
+        // action
+        let result = WeatherViewHelper.prepareLocationContent(
+                locality: initLocality,
+                administrativeArea: initAdministrativeArea,
+                country: initCountry)
+
+        //assert
+        XCTAssertEqual(result.0, expectedMainLocationString, "Incorrect mainLocationString")
+        XCTAssertEqual(result.1, expectedAdditionalLocationString, "Incorrect additionalLocationString")
+    }
+
+    func test_parse_placemark_with_no_data() {
+        // setup
+        let initLocality: String? = nil
+        let initAdministrativeArea: String? = nil
+        let initCountry: String? = nil
+
+        let expectedMainLocationString = "Unknown"
+        let expectedAdditionalLocationString = ""
+
+        // action
+        let result = WeatherViewHelper.prepareLocationContent(
+                locality: initLocality,
+                administrativeArea: initAdministrativeArea,
+                country: initCountry)
+
+        //assert
+        XCTAssertEqual(result.0, expectedMainLocationString, "Incorrect mainLocationString")
+        XCTAssertEqual(result.1, expectedAdditionalLocationString, "Incorrect additionalLocationString")
+    }
 }
